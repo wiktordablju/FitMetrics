3a3ab2872561dbc6eb6ec0bbfefb4ec32e5fa7a8
wiktordablju <wiktordablju@gmail.com>
2023-09-17 18:08:53 +0200

    17.09: kalkulator bmi i wyswietlanie porad

--- index.php
+++ index.php
@@ -18,6 +18,47 @@
 </head>
 
 <body>
+    <!-- FUNKCJE -->
+    <?php
+    function calculateBMI($weight, $height)
+    {
+        return $weight / ($height * $height);
+    }
+
+    function getTips($bmi)
+    {
+        if ($bmi < 18.5) {
+            return [
+                "Jedz więcej białka i tłuszczów.",
+                "Ćwicz regularnie, ale unikaj nadmiernego wysiłku.",
+                "Skonsultuj się z dietetykiem, aby ustalić odpowiednią dietę.",
+                "Spożywaj więcej kalorii, niż spalasz."
+            ];
+        } elseif ($bmi >= 18.5 && $bmi < 24.9) {
+            return [
+                "Kontynuuj zdrową dietę.",
+                "Regularnie ćwicz, aby utrzymać dobrą kondycję.",
+                "Unikaj stresu i dbaj o regularny sen.",
+                "Pij wystarczającą ilość wody każdego dnia."
+            ];
+        } elseif ($bmi >= 25 && $bmi < 29.9) {
+            return [
+                "Zmniejsz spożycie tłuszczów i cukrów.",
+                "Zwiększ aktywność fizyczną, dodając codzienne ćwiczenia.",
+                "Staraj się spożywać mniej kalorii, niż spalasz.",
+                "Unikaj jedzenia późno wieczorem."
+            ];
+        } else {
+            return [
+                "Skonsultuj się z lekarzem lub dietetykiem w sprawie planu odchudzania.",
+                "Unikaj przetworzonej żywności i napojów gazowanych.",
+                "Zaangażuj się w regularne ćwiczenia o wysokiej intensywności.",
+                "Uważaj na ilość spożywanych kalorii."
+            ];
+        }
+    }
+
+    ?>
 
     <div class="header">
         <div class="header-text">
@@ -90,23 +131,71 @@
             <p class="calc-text">Oblicz swoje BMI</p>
         </div>
         <div class="calc-body">
+
             <form action="" method="POST">
 
                 <label for="">Podaj swój wzrost:</label>
                 <br>
-                <input type="text" name="wzrost">
+                <input type="text" name="height">
                 <br>
                 <label for="">Podaj swoją wagę:</label>
                 <br>
-                <input type="text" name="waga">
+                <input type="text" name="weight">
                 <br>
                 <input class="btn" type="submit" value="OBLICZ">
             </form>
+
+            <?php
+            $bmi = "";
+            $output = "";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['height']) && isset($_POST['weight'])) {
+                $height = $_POST['height'] / 100;
+                $weight = $_POST['weight'];
+
+                if (is_numeric($height) && is_numeric($weight) && $height > 0 && $weight > 0) {
+                    $bmi = calculateBMi($weight, $height);
+
+                    if ($bmi < 18.5) {
+                        $output = "Masz niedowagę.";
+                    } elseif ($bmi >= 18.5 && $bmi < 24.9) {
+                        $output = "Masz prawidłową masę ciała.";
+                    } elseif ($bmi >= 25 && $bmi < 29.9) {
+                        $output = "Masz nadwagę.";
+                    } else {
+                        $output = "Masz otyłość.";
+                    }
+                } else {
+                    $output = "Wprowadź poprawne wartości.";
+                }
+            }
+
+
+            if (!empty($bmi)) {
+                echo "<div class='bmi-result'>";
+                echo "Twoje BMI wynosi: " . $bmi . "<br>";
+                echo $output;
+                echo "</div>";
+            }
+
+            ?>
+
         </div>
     </div>
     <div class="tips">
         <h1>Porady</h1>
+        <?php
+        $tips = getTips($bmi);
+
+        if (!empty($tips)) {
+            foreach ($tips as $tip) {
+                echo  $tip . "<br>";
+            }
+        }
+        ?>
+
     </div>
+
 </body>
 
 </html>
